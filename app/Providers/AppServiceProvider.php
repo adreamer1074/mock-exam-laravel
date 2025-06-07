ac8b5c9c608a9d980e9adadfff4cbc9d5f343144
maivan.quang <hogiut@gmail.com>
2025-06-07 16:43:18 +0900

    deploy to aws

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // 開発環境 (local) の場合のみ登録
+        if ($this->app->environment('local')) {
+            $this->app->register(\NunoMaduro\Collision\Adapters\Laravel\CollisionServiceProvider::class);
+        }
     }
 
     /**
